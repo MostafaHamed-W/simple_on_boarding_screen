112a1d8775f820dd82263539e78e1a961c10c359
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2023-08-31 00:21:31 +0300

    add logout functionality & reset shared preference

--- lib/views/home_view/home_view.dart
+++ lib/views/home_view/home_view.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../onboarding_view/onboarding_page_view.dart';
 
 class HomePageView extends StatelessWidget {
   const HomePageView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final pref = await SharedPreferences.getInstance();
+              pref.setBool('showHome', false);
+
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => const OnBoardingPage(),
+              ));
+            },
+            icon: const Icon(Icons.logout),
+          )
+        ],
+      ),
+      body: const SafeArea(
           child: Center(
         child: Text(
           'Home Page',
